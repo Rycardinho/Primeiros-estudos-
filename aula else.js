2abb2189b263034952de1b5dd94c7877885f8071
Ricardo Braz <106495830+Rycardinho@users.noreply.github.com>
2022-06-08 12:29:47 -0300

    Add files via upload
    auals de else e if

---
+++ aula else.js
@@ -0,0 +1,12 @@
+var vel = 59
+
+console.log("a velocidade do seu carro é ", vel)
+if (vel > 60)
+{
+    console.log("Você está muito rapido. Multado!")  
+}
+if ( vel <= 59)
+{
+    console.log("Você está dentro do Limite de Velocidade.")  
+}
+
